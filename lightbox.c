7f61abccd45914a8b99a0a992dc8b5fa5d99cecb
Colby Reinhart <colbyreinhart@gmail.com>
2022-07-19 18:42:39 -0400

    Improvements, and removed accidentally committed binary file.

--- lightbox.c
+++ lightbox.c
@@ -32,7 +32,14 @@ int main()
 			errorQuit("Couldn't read client request");
 		}
 
-		const char* request_type = strtok(request, " ");
+		// Split the request into the head and body
+		char* end_of_header = strstr(request, "\r\n\r\n");
+		*end_of_header = '\0';
+		char* request_header = request;
+		char* request_body = end_of_header + 4;
+		
+		// Get what we care about from the header (this will improve later)
+		const char* request_type = strtok(request_header, " ");
 		const char* request_route = strtok(NULL, " ");
 
 		// If this is a CONNECT call, save the socket
@@ -43,48 +50,34 @@ int main()
 				close(lightbox_socket);
 				lightbox_socket = client_connection;
 			}
+
+			continue;
 		}
-		// Otherwise, handle the request
+
+		// Otherwise, handle the request. We'll worry about the request type later
+		// Can't do anything if the esp hasn't connected
+		if (lightbox_socket == -1)
+		{
+			send_empty_response(client_connection, http_404);
+			continue;
+		}
+
+		// Get the data
+		const char* red = strtok(request_body, " ");
+		const char* green = strtok(NULL, " ");
+		const char* blue = strtok(NULL, "\r\n");
+
+		// Write the data to the socket
+		char response[100] = {0};
+		sprintf(response, "%s %s %s\n", red, green, blue);
+		if (write(lightbox_socket, response, strlen(response)) > -1)
+		{
+			send_empty_response(client_connection, http_200);
+		}
 		else
 		{
-			// Can't do anything if the esp hasn't connected
-			if (lightbox_socket == -1)
-			{
-				return send_empty_response(client_connection, http_404);
-			}
-
-			// This sucks, but I promise I'll make it better soon.
-			// I really just want to get it working.
-			char* token = strtok(NULL, "\r\n");
-			while (strstr(token, "DATA") == NULL && token != NULL)
-			{
-				token = strtok(NULL, "\r\n");
-			}
-
-			// If we couldn't find the right request body stuff
-			if (token == NULL)
-			{
-				return send_empty_response(client_connection, http_400);
-			}
-
-			// Get the data
-			strtok(token, " "); // Get rid of the "DATA" part
-			const char* red = strtok(NULL, " ");
-			const char* green = strtok(NULL, " ");
-			const char* blue = strtok(NULL, " ");
-
-			// Write the data to the socket
-			char response[100];
-			sprintf(response, "%s %s %s\n", red, green, blue);
-			if (write(lightbox_socket, response, strlen(response)) > -1)
-			{
-				return send_empty_response(client_connection, http_200);
-			}
-			else
-			{
-				perror("Couldn't write to lightbox");
-				return send_empty_response(client_connection, http_500);
-			}
+			perror("Couldn't write to lightbox");
+			send_empty_response(client_connection, http_500);
 		}
 	}
 }
